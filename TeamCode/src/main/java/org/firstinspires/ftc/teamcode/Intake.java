02005ff58d9aeb46b8478c212bc03d6606656791
Leo <leoquan09@gmail.com>
2026-09-24 16:54:11 -0700

    Update Intake.java
    added a reverse motor functionality if b is pressed. Needs to be tested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -10,17 +10,25 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
     DcMotor intakeMotor;
-    boolean run_motor = false;
+    boolean run_motor_straight = false;
+    boolean run_motor_back = false;
     public void setup(HardwareMap hardwareMap) {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
     }
 
     public void run(Gamepad gamepad) {
         if (gamepad.a) {
-            run_motor = !run_motor;
+            run_motor_straight = !run_motor_straight;
+        } else if (gamepad.b) {
+            run_motor_back = !run_motor_back;
         }
-        if (run_motor) {
+
+        if (run_motor_straight) {
             intakeMotor.setPower(1.0);
+        } else if (run_motor_back) {
+            intakeMotor.setPower(-1.0);
+        } else {
+            intakeMotor.setPower(0);
         }
     }
 }
